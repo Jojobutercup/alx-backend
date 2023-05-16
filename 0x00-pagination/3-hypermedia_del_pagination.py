ead34c21abf40fdcdf6abe6850f2d0c8a5639f19
jojobutercup <jojobutercup@gmail.com>
2023-05-16 05:54:25 +0100

    commit

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,13 +5,12 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Dict
+from typing import Dict, List
 
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -40,31 +39,18 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
-        """Return all data"""
-
-        if index is None:
-            index = 0
-
-        # validate the index
-        assert isinstance(index, int)
-        assert 0 <= index < len(self.indexed_dataset())
-        assert isinstance(page_size, int) and page_size > 0
-
-        data = []  # collect all indexed data
-        next_index = index + page_size
-
-        for value in range(index, next_index):
-            if self.indexed_dataset().get(value):
-                data.append(self.indexed_dataset()[value])
-            else:
-                value += 1
-                next_index += 1
-
-        return {
-            'index': index,
-            'data': data,
-            'page_size': page_size,
-            'next_index': next_index
-        }
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """ hyper index """
+        requested_data = []
+        next = index + page_size
+        for idx in range(index, index + page_size):
+            if not self.indexed_dataset().get(idx):
+                next += 1
+            requested_data.append(self.indexed_dataset()[idx])
+        result = {
+                  'data': requested_data,
+                  'index': index,
+                  'next_index': next,
+                  'page_size': page_size
+                  }
+        return result
