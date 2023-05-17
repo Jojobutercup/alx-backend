21dd13d69079e28d4658f0f1c0a58491c120bd94
jojobutercup <jojobutercup@gmail.com>
2023-05-17 06:05:17 +0100

    Caching Strings

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,6 +4,7 @@
 from base_caching import BaseCaching
 """import parent file"""
 
+
 class BasicCache(BaseCaching):
     """ BasicCache inherits the BaseChaching blueprint """
     def __init__(self):
@@ -12,7 +13,7 @@ class BasicCache(BaseCaching):
         of the class BasicCache is created """
         self.cache_data = {}
         """ initialized cache_data variable,
-        which is an empty dictionary used to 
+        which is an empty dictionary used to
         store data in the cache (a smaller and faster storage)
         """
 
@@ -20,7 +21,7 @@ class BasicCache(BaseCaching):
         """ define a print_cache funtion of the object cache_data"""
         print("Current cache:")
         for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key,self.cache_data.get(key)))
+            print("{}: {}".format(key, self.cache_data.get(key)))
 
     def put(self, key, item):
         """ define a put function that allows items
